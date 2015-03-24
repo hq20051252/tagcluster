739549b0cb4667921d55ea0351427339927306d2
hq20051252 <hq20051252@163.com>
2015-03-24 19:47:19 +0800

    sequence file writer

--- src/com/umessage/tagcluster/tools/ConvertToVector.java
+++ src/com/umessage/tagcluster/tools/ConvertToVector.java
@@ -19,41 +19,26 @@ import org.apache.mahout.math.VectorWritable;
 public class ConvertToVector {
 
 	public static void main(String[] args) throws Exception{
-		List<NamedVector> wordvecs = new ArrayList<NamedVector>();
-		
 		NamedVector wordvec;
 		
-		if (args.length != 3) {
+		if (args.length != 2) {
+			for (int i =0; i < args.length; i++) {
+				System.out.println("The " + new Integer(i).toString() + " argument: " + args[i]);
+			}
 			Usage();
 			System.exit(1);
 		}
-		String input = args[1];
-		String output = args[2];
+		String input = args[0];
+		String output = args[1];
 		
+		// 输入文件
 		File f = new File(input);
 		
 		FileInputStream fis = new FileInputStream(f);
 		InputStreamReader dis = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(dis);
 		
-		// 内容处理,转换成vector.
-		String line;
-		while ((line = br.readLine()) != null){
-			String[] ss = line.split(" ", -1);
-			if (ss.length != 101){
-				continue;
-			}
-			String key = ss[0];
-			double[] value = new double[101];
-			
-			for (int i = 1; i <= 100; i++) {
-				value[i] = new Double(ss[i]);
-			}
-			wordvec = new NamedVector(new DenseVector(value), key);
-			wordvecs.add(wordvec);
-		}
-		br.close();
-		
+		// 输出文件
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(conf);
 		
@@ -61,10 +46,29 @@ public class ConvertToVector {
 		SequenceFile.Writer writer = new SequenceFile.Writer(fs,  conf,  path, Text.class,  VectorWritable.class);
 		
 		VectorWritable vec = new VectorWritable();
-		for (NamedVector vector : wordvecs) {
-			vec.set(vector);
-			writer.append(new Text(vector.getName()), vec);
+		
+		// 内容处理,转换成vector.
+		int pregress = 0; // 进度指示器
+		String line;
+		while ((line = br.readLine()) != null){
+			pregress ++;
+			// 打印进度
+			System.out.print("Progress: " + new Integer(pregress).toString() + ".\r");
+			String[] ss = line.trim().split(" ", -1);
+			if (ss.length != 101){
+				continue;
+			}
+			String key = ss[0];
+			double[] value = new double[100];
+			
+			for (int i = 1; i <= 100; i++) {
+				value[i-1] = new Double(ss[i]);
+			}
+			wordvec = new NamedVector(new DenseVector(value), key);
+			vec.set(wordvec);
+			writer.append(new Text(wordvec.getName()), vec);
 		}
+		br.close();
 		writer.close();
 	}
 
